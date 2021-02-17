fb88dbb00e653d5f80710e421d3e7539f6b1ad4d
Aleksandr Kravets <a.kravets@alterego.digital>
2021-02-17 14:03:15 +0200

    style select

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,16 @@ import style from './style.module.css'
 
 const Header = ({handleChange}) => {
 
+  const options = [
+    { value: '30days', label: '30days' },
+    { value: 'Hours', label: 'Hours' },
+  ]
+
   return (
     <header className={style.header}>
         <div className={style.headerTitle}>Completed sign-ups over time</div>
         <div className={style.headerDropdown}>
-        <select onChange={(e) => handleChange(e.target.value)}>
+        <select  onChange={(e) => handleChange(e.target.value)}>
             <option value="days">30days</option>
             <option value="hours">Hours</option>
         </select>
